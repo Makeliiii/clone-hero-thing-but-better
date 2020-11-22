9d89b7b234ce74c092ca2c7b4bd5f1578a6ae74b
Mäkeli <joel.m.maenpaa@gmail.com>
2020-11-22 18:00:55 +0200

    react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 // import views
 import Main from './views/Main'
+import Charts from './views/Charts'
 
 const App = () => {
 	const [charts, setCharts] = useState([])
 	
   	return (
-		<>
-	  		<Main setCharts={setCharts} />
-		</>
+		<Router>
+			<Route exact path='/' render={props => <Main {...props} setCharts={setCharts} />} />
+			<Route exact path='/charts' render={props => <Charts {...props} charts={charts} />} />
+		</Router>
   	)
 }
 
