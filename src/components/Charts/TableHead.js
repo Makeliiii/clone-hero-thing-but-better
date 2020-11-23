3d58ceeff3ae93182fe9fb1a4ccbe19614060a1d
Mäkeli <joel.m.maenpaa@gmail.com>
2020-11-23 21:33:33 +0200

    table head component

--- src/components/Charts/TableHead.js
+++ src/components/Charts/TableHead.js
@@ -2,16 +2,18 @@ import React from 'react'
 
 const TableHead = () => {
     return (
-        <tr>
-            <th>Name</th>
-            <th>Album</th>
-            <th>Artist</th>
-            <th>Charter(s)</th>
-            <th>Genre</th>
-            <th>Year</th>
-            <th>Lyrics</th>
-            <th>Song Length</th>
-        </tr>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Album</th>
+                <th>Artist</th>
+                <th>Charter(s)</th>
+                <th>Genre</th>
+                <th>Year</th>
+                <th>Lyrics</th>
+                <th>Song Length</th>
+            </tr>
+        </thead>
     )
 }
 
